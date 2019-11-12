399c2ca6bede97840134f524e6a89e6aaa2c73e9
Alexandre <falexandrealencar@hotmail.com>
2019-11-12 14:29:07 -0300

    Implementação procedural

--- DB1_Java/src/com/db1/db1start/app.java
+++ DB1_Java/src/com/db1/db1start/app.java
@@ -2,12 +2,39 @@ package com.db1.db1start;
 import java.util.Scanner;
 
 public class app {
+	
+	public static String soma( Integer numero1, Integer numero2) {
+		Integer resultado = numero1 + numero2;
+		return "O resultado da soma é " + resultado;
+	}
+	
+	public static String maiuscula( String texto_normal) {
+		String texto_maiusculo = texto_normal.toUpperCase();
+		return "Texto Maiusculo: " + texto_maiusculo;
+	}
+	
+	public static String minuscula( String texto_normal) {
+		String texto_minusculo = texto_normal.toLowerCase();
+		return "Texto Minusculo: " + texto_minusculo;
+	}
+	
+	public static String menor_valor(double valor1, double valor2, double valor3) {
+		if (valor1 < valor2 && valor1 < valor3) {
+			return ("valor 1 é o menor");
+		}else if (valor2 < valor3 && valor2 < valor1) {
+			return ("valor 2 é o menor");
+		}else return ("valor 3 é o menor");
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Integer numero1, resultado;
-		Integer numero2;
+		Integer numero1, numero2;
 		String texto_normal;
+		String resultado;
+		String texto_maiusculo;
+		String texto_minusculo;
+		String resultado_menor_valor;
 	
 		System.out.println("Digite um numero");
 		Scanner scan_numero1 = new Scanner(System.in);
@@ -17,17 +44,18 @@ public class app {
 		Scanner scan_numero2 = new Scanner(System.in);
 		numero2 = scan_numero2.nextInt();
 		
-		resultado = numero1 + numero2;
-		System.out.println("O resultado da soma é: " + resultado);
+		resultado = soma(numero1,numero2);
+		System.out.println(resultado);
 		
 		System.out.println("Digite um texto");
 		Scanner scan_texto = new Scanner(System.in);
 		texto_normal = scan_texto.next();
-		String texto_maiusculo = texto_normal.toUpperCase();
-		String texto_minusculo = texto_normal.toLowerCase();
 		
-		System.out.println("Texto Minusculo: " + texto_minusculo);
-		System.out.println("Texto Maiusculo: " + texto_maiusculo);
+		texto_maiusculo = maiuscula(texto_normal);
+		texto_minusculo = minuscula(texto_normal);
+		
+		System.out.println(texto_maiusculo);
+		System.out.println(texto_minusculo);
 		
 		System.out.println("Digite o valor 1");
 		Scanner scan_valor1 = new Scanner(System.in);
@@ -41,11 +69,8 @@ public class app {
 		Scanner scan_valor3 = new Scanner(System.in);
 		double valor3 = scan_valor3.nextInt();
 		
-		if (valor1 < valor2 && valor1 < valor3) {
-			System.out.println("valor 1 é o menor");
-		}else if (valor2 < valor3 && valor2 < valor1) {
-			System.out.println("valor 2 é o menor");
-		}else System.out.println("valor 3 é o menor");
+		resultado_menor_valor=menor_valor(valor1,valor2,valor3);
+		System.out.println(resultado_menor_valor);
 	}
 
 }
