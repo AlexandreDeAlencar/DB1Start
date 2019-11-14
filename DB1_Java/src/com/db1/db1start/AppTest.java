7777d3050e8b04f9aa365179fcdb7e917d0db3a4
Alexandre <falexandrealencar@hotmail.com>
2019-11-14 10:56:30 -0300

    Excercício Aula07 e teste unitário de todos os métodos implementados

--- DB1_Java/src/com/db1/db1start/AppTest.java
+++ DB1_Java/src/com/db1/db1start/AppTest.java
@@ -2,24 +2,190 @@ package com.db1.db1start;
 
 import org.junit.Test;
 
-import com.sun.tools.javac.util.Assert;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
-	
+
+Application application = new Application();
+
 	@Test
-	public void showNumber5() {
-		Application application = new Application();
+	public void showSoma5() {
 		Integer expected=5;
 		Integer response = application.soma(2,3);
-		Assert.assertEquals(expected, response);
+		assertEquals(expected, response);
 	}
-	
+
 	@Test
-	public void showNumber8() {
-		Application application = new Application();
-		Integer expected=5;
-		Integer response = application.soma(2,3);
-		Assert.assertEquals(expected, response);
+	public void showSubtracao5() {
+		double expected=4.0;
+		double response;
+		response = application.subtracao(5, 1);
+		assertEquals(expected, response, 0.0);
 	}
-	
+
+
+	@Test
+	public void showMultiplica8() {
+		double expected=8.0;
+		double response;
+		response = application.multiplica(2, 4);
+		assertEquals(expected, response, 0.0);
+	}
+
+	@Test
+	public void showDivide8() {
+		double expected=8.0;
+		double response;
+		response = application.divide(16, 2);
+		assertEquals(expected, response, 0.0);
+	}
+
+	@Test
+	public void showPar() {
+		String expected= "Par";
+		String response;
+		response = application.par(16);
+		assertEquals(expected, response);
+	}
+
+	@Test
+	public void showMaiorValor8() {
+		double expected=8.0;
+		double response;
+		response = application.maiorValor(1, 8);
+		assertEquals(expected, response, 0.0);
+	}
+
+	@Test
+	public void showQuantidadeNImpares8() {
+		double expected=8;
+		double response;
+		response = application.quantidadeNImpares(15);
+		assertEquals(expected, response, 0.0);
+	}
+
+	@Test
+	public void showMaiusculaALEX() {
+		String expected= "ALEX";
+		String response;
+		response = application.maiuscula("AleX");
+		assertEquals(expected, response);
+	}
+
+	@Test
+	public void showMinusculaAlex() {
+		String expected= "alex";
+		String response;
+		response = application.minuscula("AleX");
+		assertEquals(expected, response);
+	}
+
+	@Test
+	public void showContaCaracteresDB1START() {
+		int expected= 8;
+		int response;
+		response = application.contaCaracteres("DB1START");
+		assertEquals(expected, response, 0.0);
+	}
+
+	@Test
+	public void showContaLetrasDB1START() {
+		int expected= 8;
+		int response;
+		response = application.contaLetras(" DB1START ");
+		assertEquals(expected, response, 0.0);
+	}
+
+	@Test
+	public void showExibe4PrimeirasLetrasDB1S() {
+		String expected= "DB1S";
+		String response;
+		response = application.exibe4PrimeirasLetras(" DB1 START ");
+		assertEquals(expected, response);
+
+	}
+
+	@Test
+	public void showExibeAPartirda3PrimeiraLetra1START() {
+		String expected= "1START";
+		String response;
+		response = application.exibeAPartirda3PrimeiraLetra(" DB1 START ");
+		assertEquals(expected, response);
+	}
+
+	@Test
+	public void showExibe4UltimasLetrasTART() {
+		String expected= "TART";
+		String response;
+		response = application.exibe4UltimasLetras(" DB1 START ");
+		assertEquals(expected, response);
+	}
+
+	@Test
+	public void showsubstituiPrimeiroNome() {
+		String expected= "ALUNO/ALUNA DE ALENCAR FERREIRA";
+		String response;
+		response = application.substituiPrimeiroNome("ALEXANDRE DE ALENCAR FERREIRA");
+		assertEquals(expected, response);
+
+	}
+
+	@Test
+	public void showExibeTextoSeparadamenteMelancia() {
+		String expected = "melancia";
+		String[] response;
+		response = application.exibeTextoSeparadamente("banana, maçã, melancia");
+		assertEquals(expected, response[2]);
+
+	}
+
+    @Test
+    public void showQuantidadeVogais() {
+        int expected = 4;
+        int response;
+        response = application.quantidadeVogais("melancia");
+        assertEquals(expected, response);
+
+    }
+
+    @Test
+    public void showInverteStringAicalem() {
+        String  expected = "aicnalem";
+        String response;
+        response = application.inverteString("melancia");
+        assertEquals(expected, response);
+
+    }
+
+    @Test
+    public void showMenorValorEntre2Doubles() {
+        double expected = 2;
+        double  response;
+        response = application.menorValor(5,2);
+        assertEquals(expected, response, 0.0);
+    }
+
+    @Test
+    public void showMenorValorEntre3Doubles() {
+        double expected = 2;
+        double  response;
+        response = application.menorValor(5,2,4);
+        assertEquals(expected, response, 0.0);
+    }
+
+    @Test
+    public void showMediaEntre3Doubles() {
+        double expected = 4;
+        double  response;
+        response = application.media3Valores(6,2,4);
+        assertEquals(expected, response, 0.0);
+    }
+
+    @Test
+    public void showAreaTriangulo() {
+        double expected = 6;
+        double  response;
+        response = application.areaTriangulo(6,2);
+        assertEquals(expected, response, 0.0);
+    }
 }
---
+++ DB1_Java/src/com/db1/db1start/Application.java
@@ -0,0 +1,160 @@
+package com.db1.db1start;
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+	
+	public Integer soma( Integer numero1, Integer numero2) {
+
+		return numero1+numero2;
+	}
+	
+	public Integer menorValor(Integer valor1, Integer valor2) {
+		if (valor1 < valor2) {
+			return valor1;
+		}
+			return valor2;
+	}
+	
+	public Integer subtracao(Integer valor1, Integer valor2) {
+			return valor1 - valor2;
+	}
+	public Integer multiplica(Integer valor1, Integer valor2) {
+		return valor1*valor2;
+	}
+
+	public Integer divide(Integer valor1, Integer valor2) {
+		return valor1/valor2;
+	}
+	
+	public String par(Integer valor) {
+		if (valor%2==0) {
+			return "Par";
+		}
+		return "Impar";
+	}
+	
+	public Integer maiorValor(Integer valor1, Integer valor2) {
+		if (valor1 > valor2) {
+			return valor1;
+		}
+			return valor2;
+	}
+	
+	public Integer quantidadeNImpares(Integer valor) {
+		Integer resto = valor%2;
+		return resto + valor/2;
+	}
+	
+	public String maiuscula( String texto_normal) {
+		return texto_normal.toUpperCase();
+	}
+	
+	public String minuscula( String texto_normal) {
+		String texto_minusculo = texto_normal.toLowerCase();
+		return texto_minusculo;
+	}
+	
+	public int contaCaracteres (String palavra) {
+		return palavra.length();
+	}
+	
+	public int contaLetras (String palavra) {
+		palavra = palavra.replaceAll("\\s+","");
+		return palavra.length();
+	}
+	
+	public String limpaEspacoEmBranco (String palavra) {
+			palavra = palavra.replaceAll("\\s+","");
+			return palavra;
+	}
+	
+	public String exibe4PrimeirasLetras (String palavra) {
+		palavra = palavra.replaceAll("\\s+","");
+		String quatro_primeiras_letras="" ;
+		    for(int i =0; i < 4; i++){
+		        quatro_primeiras_letras = quatro_primeiras_letras + palavra.charAt(i);      
+		    }
+		return quatro_primeiras_letras;
+}
+	
+	public String exibeAPartirda3PrimeiraLetra (String palavra) {
+		palavra = palavra.replaceAll("\\s+","");
+		String resultado="" ;
+		    for(int i =2; i < palavra.length(); i++){
+		        resultado = resultado + palavra.charAt(i);      
+		    }
+		return resultado;
+}
+	
+	public String exibe4UltimasLetras (String palavra) {
+		palavra = palavra.replaceAll("\\s+","");
+		String quatro_ultimas_letras="" ;
+		    for(int i = 3; i >= 0; i--){
+		    	int indx=palavra.length()-i-1;
+		        quatro_ultimas_letras = quatro_ultimas_letras + palavra.charAt(indx);      
+		    }
+		return quatro_ultimas_letras;
+}
+	
+	public String substituiPrimeiroNome(String palavra) {
+		int count=0;
+		String sobrenome ="";
+		while (palavra.charAt(count) != ' '){
+			count++;
+	    }
+		
+		for(int i = count; i < palavra.length(); i++){
+	        sobrenome = sobrenome + palavra.charAt(i);      
+	    }
+		
+		String aluno = "ALUNO/ALUNA" + sobrenome;
+		return aluno;
+}
+	public String[] exibeTextoSeparadamente(String palavra) {
+		return palavra.split(", ");
+}
+	
+	public int quantidadeVogais(String palavra) {
+		String[] vogais = new String[] {"a","e","i","o","u"};
+		
+		// Convert String Array to List
+        List<String> list = Arrays.asList(vogais);
+       
+		int count=0;
+		for(int i = 0; i < palavra.length(); i++){
+			if (list.contains(Character.toString(palavra.charAt(i)))) {
+	        count++;      
+	    }
+		}
+		
+		return count;
+}
+	public String inverteString(String palavra) {
+		String resultado = "";
+		int count=0;
+		for(int i = palavra.length()-1; i >=0 ; i--){
+			resultado= resultado + palavra.charAt(i);
+	    }
+		return resultado;
+}
+	public double menorValor(double valor1, double valor2) {
+		if (valor1 < valor2) {
+			return valor1;
+		}
+			return valor2;
+	}
+	
+	public double menorValor(double valor1, double valor2, double valor3) {
+		return menorValor(menorValor(valor1,valor2),valor3);
+	}
+	
+	public double media3Valores(double valor1, double valor2, double valor3) {
+		return (valor1+valor2+valor3)/3;
+	}
+	
+	public double areaTriangulo(double base, double altura) {
+
+		return (base*altura)/2;
+	}
+}
